4b22c1d77a59c7a61348a56e4439b1b791fc469e
razzak118348 <abdurrazzak118348@gmail.com>
2024-08-01 18:43:29 +0600

    famous writer

--- src/Components/FamousWriter/FamousWriter.jsx
+++ src/Components/FamousWriter/FamousWriter.jsx
@@ -1,8 +1,24 @@
 
 const FamousWriter = () => {
+    const writers=[
+        "F. Scott Fitzgerald",
+        "Harper Lee",
+        "George Orwell",
+        "Jane Austen",
+        "J.D. Salinger",
+        "J.R.R. Tolkien",
+        "Herman Melville",
+        "Leo Tolstoy",
+        "Paulo Coelho"
+    ]
     return (
         <div>
             <h2 className="text-4xl">Famous Writer</h2>
+            <ul className="text-xl font-bold mt-8 mb-8">
+            {
+                writers.map((writer,idx)=><li className="my-4 " key={idx}>{writer}</li>)
+            }
+            </ul>
         </div>
     );
 };
